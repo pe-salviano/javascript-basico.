75d70cc7e3eea3dc2c607fa35c78b5a3e57e702b
pe-salviano <pedro_s.c@hotmail.com>
2022-06-14 20:19:19 -0300

    atualização

--- Repeticao-for-while/script.js
+++ Repeticao-for-while/script.js
@@ -2,6 +2,6 @@ var numero = 5;
 
 for (var vez = 0; vez < numero; vez++){
 
-    console.log("Executando o for")
+    console.log("Executando o for, pela " + vez + " vez")
 }
 console.log ("Acabou");
---
+++ arrays/script.js
@@ -0,0 +1,43 @@
+// var alunos = new Array ("Pedro", "Jéssica", "Dalva", "Lyon");
+
+// console.log (alunos[0]);
+
+// --------------------------------------------------------------------------
+
+// var alunos = ["Pedro", "Jéssica", "Dalva", "Lyon"];
+
+//  console.log (alunos.length);
+
+// var aluno1 = "Pedro";
+// var aluno2 = "Jéssica";
+// var aluno3 = "Pedro";
+// var aluno4 = "Jéssica";
+// var aluno5 = "Pedro";
+
+// console.log (aluno1);
+// console.log (aluno2);
+// console.log (aluno3);
+// console.log (aluno4);
+// console.log (aluno5);
+
+// ----------------------------------------------------------------------------
+// var alunos = ['Pedro', 'Jéssica', 'Lyon', 'Dalva', 'Leo', 'Ana'];
+
+// console.log (alunos[0]);
+// console.log (alunos[1]);
+// console.log (alunos[2]);
+// console.log (alunos[3]);
+// console.log (alunos[4]);
+// console.log (alunos[5]);
+
+// ------------------------------------------------------------------------------
+
+var alunos = ['Pedro', 'Jéssica', 'Lyon', 'Dalva', 'Leo', 'Ana'];
+
+for (var i = 0; i < alunos.length; i++)
+{
+    console.log(alunos[i]);
+
+}
+
+
