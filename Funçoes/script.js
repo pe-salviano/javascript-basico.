47f6985b44b8d85d9face0ccd4e46ff463ae1b66
pe-salviano <pedro_s.c@hotmail.com>
2022-06-14 21:11:59 -0300

    atualização funcoes

---
+++ Funçoes/script.js
@@ -0,0 +1,32 @@
+// function media (n1, n2) {
+
+//     var nota1 = n1;
+//     var nota2 = n2;
+//     var media = (nota1 + nota2) /2;
+//     // console.log(media);
+
+//     return media;
+// }
+
+// var resultado1 = media (6, 8);
+// var resultado2 = media (8, 9);
+
+// console.log(resultado1 + resultado2);
+
+// ------------------------------------------------------------------------------
+
+// function saudacao(){
+
+//     return "Olá Mundo!";
+// }
+
+// var s = saudacao ();
+
+// console.log(s);
+
+// -----------------------------------------------------------------------
+
+var media = function (n1,n2){
+    return (n1 + n2) / 2;
+}
+console.log(media (5, 6));
