d10a2a731d5276a952ad409006b12b3787abc7c1
pe-salviano <pedro_s.c@hotmail.com>
2022-06-15 12:10:18 -0300

    Atualização Objetos - metodos Javascript

---
+++ Objetos-propriedades-metodos-construtores/script.js
@@ -0,0 +1,47 @@
+// Objeto
+
+// var aluno = {nome: 'Igor', notas:[7.5, 5.0]
+
+// }
+
+// console.log (aluno["notas"][1]);
+
+// var NovaPropriedade = "Sobrenome";
+
+// aluno.matricula = 12345;
+
+// aluno[NovaPropriedade] = "Oliveira";
+
+// console.log(aluno)
+
+// ---------------------------------------------------------------------------
+
+
+// Obrjetos - métodos
+
+// var aluno = {
+//     nome:"Igor", notas: [7, 8],
+
+//     media: function (n1, n2) {
+//         return (n1 + n2) / 2;
+//     }
+
+// }
+
+// var aluno1 = {
+//     nome:"João", notas: [8, 8],
+
+//     media: function (n1, n2) {
+//         return (n1 + n2) / 2;
+//     }
+
+// }
+
+// console.log(aluno.nome)
+// console.log(aluno.media(aluno.notas[0], aluno.notas[1]));
+
+
+// console.log(aluno1.nome)
+// console.log(aluno.media(aluno1.notas[0], aluno1.notas[1]));
+
+//--------------------------------------------------------------------------
